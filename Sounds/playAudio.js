ea4410dd33def8a1386c755475f373fe4455e2fd
balanaa <92966607+balanaa@users.noreply.github.com>
2025-08-13 23:56:32 +0800

    Add files via upload

--- Sounds/playAudio.js
+++ Sounds/playAudio.js
@@ -1,10 +1,11 @@
-const audio = new Audio('Sounds/thud_bloody.mp3');
+const audio = new Audio('Sounds/thud.mp3');
 
 document.querySelectorAll('.sideButtons').forEach(link => {
     link.addEventListener('click', event => {
         event.preventDefault();
         audio.play();
+        //audio.volume = 0.1;
 
-        setTimeout(() => { window.location.href = link.href; }, 600);
+        setTimeout(() => { window.location.href = link.href; }, 800);
     });
 });
--- clock.js
+++ clock.js
@@ -4,18 +4,28 @@ function realTimeClock(){
        var hours = date.getHours();
        var minutes = date.getMinutes();
        var seconds = date.getSeconds();
+       let periodOfTheDay;
 
        //var time = hours+":"+minutes+":"+seconds;
 
+       if(hours > 0 && hours < 12){
+              periodOfTheDay = "AM";
+       }else if (hours == 12) {
+              periodOfTheDay = "NN"
+       }else if (hours > 12 && hours < 25){
+              periodOfTheDay = "PM"
+       } else {
+              console.log("periodOfTheDay : Out of Bounds")
+       }
        
        if(hours > 12){
               hours-=12;
        }
-
+              
        minutes = ("0" + minutes).slice(-2);
        seconds = ("0" + seconds).slice(-2);
 
        document.getElementById('clock').innerHTML = 
-              hours + " : " + minutes + " : " + seconds;
+              hours + " : " + minutes + " : " + seconds + " " + periodOfTheDay;
               var time = setTimeout(realTimeClock, 500);
 }
